65a1705e5097118b13e1ab6b083d0d1c19e08bd9
Genjikuh <javier.tello85@gmail.com>
2020-06-07 14:22:21 +0200

    progressing more datatypes lab

--- 03_more_datatypes/3_tuples/03_12_list_to_tuple.py
+++ 03_more_datatypes/3_tuples/03_12_list_to_tuple.py
@@ -2,3 +2,8 @@
 Write a script that takes a list and turns it into a tuple.
 
 '''
+
+ask_string = input("Tell me something: ")
+list1 = ask_string.split()
+tuple1 = tuple(list1)
+print(tuple1)
--- 03_more_datatypes/3_tuples/03_13_tuple_to_list.py
+++ 03_more_datatypes/3_tuples/03_13_tuple_to_list.py
@@ -2,3 +2,9 @@
 Write a script that takes a tuple and turns it into a list.
 
 '''
+
+tuple1 = 5, 6, 7, 8
+print(tuple1)
+
+list1 = list(tuple1)
+print(list1)
